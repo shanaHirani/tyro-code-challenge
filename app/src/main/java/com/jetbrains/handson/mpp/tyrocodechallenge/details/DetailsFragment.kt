ee4eddf60849ac15f124059d0e7fff4709e5ef84
shanaHirani <shana.hirani@gmail.com>
2021-09-16 15:59:46 +1000

    change folder

--- app/src/main/java/com/jetbrains/handson/mpp/tyrocodechallenge/details/DetailsFragment.kt
+++ app/src/main/java/com/jetbrains/handson/mpp/tyrocodechallenge/details/DetailsFragment.kt
@@ -1,19 +1,20 @@
-package com.jetbrains.handson.mpp.tyrocodechallenge
+package com.jetbrains.handson.mpp.tyrocodechallenge.details
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.jetbrains.handson.mpp.tyrocodechallenge.R
 
-class MovieListFragment : Fragment() {
+class DetailsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_movie_list, container, false)
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_details, container, false)
     }
 
 }
--- app/src/main/java/com/jetbrains/handson/mpp/tyrocodechallenge/movieList/MovieListFragment.kt
+++ app/src/main/java/com/jetbrains/handson/mpp/tyrocodechallenge/movieList/MovieListFragment.kt
@@ -1,19 +1,20 @@
-package com.jetbrains.handson.mpp.tyrocodechallenge
+package com.jetbrains.handson.mpp.tyrocodechallenge.movieList
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.jetbrains.handson.mpp.tyrocodechallenge.R
 
-class DetailsFragment : Fragment() {
+class MovieListFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false)
+
+        return inflater.inflate(R.layout.fragment_movie_list, container, false)
     }
 
 }
