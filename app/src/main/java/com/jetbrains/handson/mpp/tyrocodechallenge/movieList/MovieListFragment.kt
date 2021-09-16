9dcf2b6425e37147a5ddda834a87d9b197e292b8
shanaHirani <shana.hirani@gmail.com>
2021-09-16 17:20:03 +1000

    view model

--- app/src/main/java/com/jetbrains/handson/mpp/tyrocodechallenge/movieList/MovieListFragment.kt
+++ app/src/main/java/com/jetbrains/handson/mpp/tyrocodechallenge/movieList/MovieListFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.jetbrains.handson.mpp.tyrocodechallenge.R
 
 class MovieListFragment : Fragment() {
+
+    private val viewModel: MovieListViewModel by lazy {
+        ViewModelProvider(this).get(MovieListViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
