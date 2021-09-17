9d7ad02e36beaab9d948d96a665982d66ffff669
shanaHirani <shana.hirani@gmail.com>
2021-09-17 23:28:44 +1000

    search menue

--- app/src/main/java/com/jetbrains/handson/mpp/tyrocodechallenge/MainActivity.kt
+++ app/src/main/java/com/jetbrains/handson/mpp/tyrocodechallenge/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.jetbrains.handson.mpp.tyrocodechallenge
 
+import android.app.SearchManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.databinding.DataBindingUtil
 import com.jetbrains.handson.mpp.tyrocodechallenge.databinding.ActivityMainBinding
 
@@ -15,5 +22,28 @@ class MainActivity : AppCompatActivity() {
             R.layout.activity_main
         )
         getSupportActionBar()?.setDisplayShowTitleEnabled(false)
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val inflater = menuInflater
+        inflater.inflate(R.menu.search_menu, menu)
+        val manager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val search = menu?.findItem(R.id.search_movie)
+        val searchview = search?.actionView as SearchView
+        searchview.setSearchableInfo(manager.getSearchableInfo(componentName))
+        searchview.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchview.clearFocus()
+                searchview.setQuery("",false)
+                search.collapseActionView()
+                query?.let { Log.i("sss", it) }
+                return true
+            }
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+        return true
     }
 }
