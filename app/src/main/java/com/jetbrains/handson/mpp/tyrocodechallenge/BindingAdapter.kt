b9b70d2339ff9c6254c05a92c7b30e6c8b962d9e
shanaHirani <shana.hirani@gmail.com>
2021-09-18 22:09:41 +1000

    error handeling

--- app/src/main/java/com/jetbrains/handson/mpp/tyrocodechallenge/BindingAdapter.kt
+++ app/src/main/java/com/jetbrains/handson/mpp/tyrocodechallenge/BindingAdapter.kt
@@ -6,6 +6,7 @@ import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.jetbrains.handson.mpp.tyrocodechallenge.movieList.ApiStatus
 import com.jetbrains.handson.mpp.tyrocodechallenge.movieList.MovieListAdapter
 import com.jetbrains.handson.mpp.tyrocodechallenge.netWork.Movie
@@ -16,6 +17,10 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
         val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
         Glide.with(imgView.context)
             .load(imgUri)
+            .apply(
+                RequestOptions()
+                .placeholder(R.drawable.loading_animation)
+                .error(R.drawable.ic_broken_image))
             .into(imgView)
     }
 }
--- app/src/main/java/com/jetbrains/handson/mpp/tyrocodechallenge/details/DetailViewModel.kt
+++ app/src/main/java/com/jetbrains/handson/mpp/tyrocodechallenge/details/DetailViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.*
 import com.jetbrains.handson.mpp.tyrocodechallenge.netWork.Movie
 import com.jetbrains.handson.mpp.tyrocodechallenge.data.model.MovieDetail
 import com.jetbrains.handson.mpp.tyrocodechallenge.data.model.repository.Repository
+import com.jetbrains.handson.mpp.tyrocodechallenge.movieList.ApiStatus
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -13,22 +14,29 @@ class DetailViewModel @Inject constructor (
     private val repository: Repository,
     var state: SavedStateHandle
     ):ViewModel() {
+
     private val _selectedMovie = MutableLiveData<MovieDetail>()
     val selectedMovie: LiveData<MovieDetail>
         get() = _selectedMovie
 
+    private val _status = MutableLiveData<ApiStatus>()
+    val status: LiveData<ApiStatus>
+        get() = _status
 
     init {
         val movie= state.get<Movie>("selectedMovie")
         movie?.let { getMovieDetail(it.title) }
+        _status.value = ApiStatus.LOADING
     }
 
     private fun getMovieDetail(title:String) {
         viewModelScope.launch {
             try {
+                _status.value = ApiStatus.LOADING
                 _selectedMovie.value = repository.getMovieDetail(title).await()
+                _status.value = ApiStatus.DONE
             } catch (e: Exception) {
-
+                _status.value = ApiStatus.ERROR
             }
         }
     }
