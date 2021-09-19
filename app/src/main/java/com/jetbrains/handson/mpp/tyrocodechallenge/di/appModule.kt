1c3a0c2a45868a080b6214b40b1690a1c40f1d10
shanaHirani <shana.hirani@gmail.com>
2021-09-19 16:16:52 +1000

    final

--- app/src/main/java/com/jetbrains/handson/mpp/tyrocodechallenge/di/appModule.kt
+++ app/src/main/java/com/jetbrains/handson/mpp/tyrocodechallenge/di/appModule.kt
@@ -6,7 +6,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-
+//
 @Module
 @InstallIn(SingletonComponent::class)
 class appModule {
